31f9e6116b870e2438ed148d3a56c1e07fa7ab08
ClifftonS <75228978+ClifftonS@users.noreply.github.com>
2023-08-03 11:05:43 +0700

    commit

--- ExploreMC3/ContentView.swift
+++ ExploreMC3/ContentView.swift
@@ -7,38 +7,60 @@
 
 import SwiftUI
 import RealityKit
-import SceneKit
+import ARKit
 
 struct ContentView : View {
+    @State private var taprecog = false
     var body: some View {
-        ZStack {
-            ARViewContainer().edgesIgnoringSafeArea(.all)
-            VStack{
-                Spacer()
-                SceneView(scene: SCNScene(named: "Canon.usdz"), options: [.autoenablesDefaultLighting, .allowsCameraControl]).frame(width: .infinity, height: UIScreen.main.bounds.height / 4).background(.white.opacity(0.3)).foregroundColor(.white.opacity(0.3)).offset(x: 0, y: 50)
-            }
-            
+        ARViewContainer(taprecog: $taprecog).onTapGesture {
+            taprecog = true
         }
+        
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
+    @Binding var taprecog: Bool
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        let arView = ARView()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        arView.session.run(config)
+//        let boxAnchor = try! Experience.loadBall()
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
+//        arView.scene.anchors.append(boxAnchor)
+        let modelEntity = try! Boxtumpuk.loadBox()
+        let anchorEntity = AnchorEntity()
+        anchorEntity.addChild(modelEntity)
+        arView.scene.addAnchor(anchorEntity)
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if taprecog == true{
+            let ball = try! Experience.loadBall()
+            
+            let cameraAnchor = AnchorEntity(.camera)
+            cameraAnchor.addChild(ball)
+            uiView.scene.addAnchor(cameraAnchor)
+//            let offsetFromCamera: SIMD3<Float> = [0, 0, -1] // Adjust the offset as needed
+//            ball.transform.translation = offsetFromCamera
+            
+            
+//            anchorEntity.addChild(modelEntity)
+//            uiView.scene.addAnchor(cameraAnchor)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                uiView.scene.removeAnchor(cameraAnchor)
+                taprecog = false
+                
+            }
+        }
+    }
     
 }
 
