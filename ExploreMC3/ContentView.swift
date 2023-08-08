303669312f9938dcbe706f5cdfd5aabaade2d60e
Bat <banthony@student.ciputra.ac.id>
2023-08-08 19:40:29 +0700

    Update ContentView.swift

--- ExploreMC3/ContentView.swift
+++ ExploreMC3/ContentView.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
     
     func placeObject(named entityName: String, for anchor: ARAnchor){
         let cannonEntity = try! ModelEntity.load(named: entityName)
-        let anchorEntity = try! AnchorEntity(anchor: anchor)
+        let anchorEntity = try! AnchorEntity()
         anchorEntity.addChild(cannonEntity)
         arView.scene.addAnchor(anchorEntity)
     }
